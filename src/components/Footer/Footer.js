30e970913d0ef63f65839a6b69170612aac1b939
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-22 16:19:05 +0100

    add:searchbar and footer mustafa

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="copyrightSign">@  Copyright by Fred Alicia Mustafa</footer>
+        </div>
+    )
+}
+
+export default Footer; 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import './SearchBar.css'
 
-function SearchBar() {
-    return (
-        <div>
+const SearchBar = () => (
+    <div className="input-wrapper">
+    <form action="/" method="get">
+        <label htmlFor="header-search">
             
-        </div>
-    )
-}
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            className="search-bar"
+            placeholder="Search blog posts"
+            name="s" 
+        />
+        <button type="submit" className="search-bar">Search</button>
+    </form>
+    </div>
+);
 
 export default SearchBar
